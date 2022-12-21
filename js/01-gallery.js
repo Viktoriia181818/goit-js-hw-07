1a2cc30d5d8d7bc7abbfda995632c77c6fb2d347
Viktoriia181818 <vcibulskaa4@gmail.com>
2022-12-21 10:17:40 +0200

    ;D

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,47 +1,47 @@
 import { galleryItems } from './gallery-items.js'
 
 console.log(galleryItems)
- // Change code below this line
+// Change code below this line
 
-const gallery = document.querySelector('.gallery')
-const galleryEl = []
+const gallery = document.querySelector('.gallery');
+const items = [];
 
 galleryItems.forEach(element => {
-	const galleryItem = document.createElement('div')
-	galleryItem.className = 'gallery--item'
-	const galleryOriginal = document.createElement('a')
-    galleryOriginal.className = 'gallery--original';
+    const galleryItem = document.createElement('div');
+    galleryItem.className = 'gallery__item';
+    const galleryOriginal = document.createElement('a');
+    galleryOriginal.className = 'gallery__link';
     galleryOriginal.href = element.original;
     const galleryImage = document.createElement('img');
     galleryImage.className = 'gallery__image';
     galleryImage.src = element.preview;
-    galleryImage.setAttribute('data-source', element.original);
+    galleryImage.setAttribute('data-source', element.original)
     galleryImage.alt = element.description;
 
     galleryItem.append(galleryOriginal);
     galleryOriginal.append(galleryImage);
-    galleryEl .push(galleryItem);
+    items.push(galleryItem);
 })
 
-gallery.append(...galleryEl );
+gallery.append(...items);
 
 gallery.addEventListener('click', e => {
     e.preventDefault();
-    if (e.target.nodeName !== 'img') {
-        return;
-	}
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
 
     const selectedImage = e.target.getAttribute('data-source');
 
     const instance = basicLightbox.create(`
     <img src="${selectedImage}" width="800" height="600">
-`)
+`);
 
-    instance.show();
+    instance.show()
     
     gallery.addEventListener('keydown', e => {
-		if (e.key === 'Escape') {
-            instance.close();
-		}
-	})
-})
+        if (e.key === 'Escape') {
+            instance.close()
+        }
+    });
+});
