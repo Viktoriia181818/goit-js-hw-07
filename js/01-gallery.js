9cee21157a88ac4b02d0c0811ad5b9cee056a2eb
Viktoriia181818 <vcibulskaa4@gmail.com>
2022-12-21 09:37:22 +0200

    д

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,48 +3,45 @@ import { galleryItems } from './gallery-items.js'
 console.log(galleryItems)
  // Change code below this line
 
-const galleryEl = document.querySelector('.gallery');
-const items = []
+const gallery = document.querySelector('.gallery')
+const galleryEl = []
 
-// const gallery = document.querySelector('.gallery')
-// const items = []
+galleryItems.forEach(element => {
+	const galleryItem = document.createElement('div')
+	galleryItem.className = 'gallery--item'
+	const galleryOriginal = document.createElement('a')
+    galleryOriginal.className = 'gallery--original';
+    galleryOriginal.href = element.original;
+    const galleryImage = document.createElement('img');
+    galleryImage.className = 'gallery__image';
+    galleryImage.src = element.preview;
+    galleryImage.setAttribute('data-source', element.original);
+    galleryImage.alt = element.description;
 
-// galleryItems.forEach(element => {
-// 	const galleryItem = document.createElement('div')
-// 	galleryItem.className = 'gallery__item'
-// 	const galleryLink = document.createElement('a')
-// 	galleryLink.className = 'gallery__link'
-// 	galleryLink.href = element.original
-// 	const galleryImage = document.createElement('img')
-//     galleryImage.className = 'gallery__image'
-//     galleryImage.src = element.preview;
-//     galleryImage.setAttribute('data-source', element.original)
-//     galleryImage.alt = element.description;
+    galleryItem.append(galleryOriginal);
+    galleryOriginal.append(galleryImage);
+    galleryEl .push(galleryItem);
+})
 
-// 	galleryItem.append(galleryLink)
-// 	galleryLink.append(galleryImage)
-// 	items.push(galleryItem)
-// })
+gallery.append(...galleryEl );
 
-// gallery.append(...items)
+gallery.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'img') {
+        return;
+	}
 
-// gallery.addEventListener('click', e => {
-//     e.preventDefault();
-//     if (e.target.nodeName !== 'IMG') {
-// 		return
-// 	}
+    const selectedImage = e.target.getAttribute('data-source');
 
-//     const selectedImage = e.target.getAttribute('data-source')
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`)
 
-//     const instance = basicLightbox.create(`
-//     <img src="${selectedImage}" width="800" height="600">
-// `)
-
-//     instance.show()
+    instance.show();
     
-//     gallery.addEventListener('keydown', e => {
-// 		if (e.key === 'Escape') {
-// 			instance.close()
-// 		}
-// 	})
-// })
+    gallery.addEventListener('keydown', e => {
+		if (e.key === 'Escape') {
+            instance.close();
+		}
+	})
+})
